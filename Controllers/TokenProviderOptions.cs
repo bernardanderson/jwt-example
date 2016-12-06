e7af8f1c524d6ef969c8731f88f26ea253fb32c5
bernardanderson <bernard.anderson@gmail.com>
2016-12-06 10:36:17 -0600

    Added simple Token Provider Options

---
+++ Controllers/TokenProviderOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace jwtStormpath.Controllers
+{
+    public class TokenProviderOptions
+    {
+        public string Path { get; set; } = "/token";
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public TimeSpan Expiration { get; set; } = TimeSpan.FromDays(1);
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+}
